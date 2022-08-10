46f7a1da2683ee357908579bcc079dc0ef432ef5
Adam Khader <92986778+mafiono@users.noreply.github.com>
2022-08-10 16:12:55 +1000

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <HomePage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from "@/components/HomePage";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePage,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="container">
+  <iframe src="https://nuxbet.com/" full-screen="true" width="100%" height="100%"></iframe>
+</div>
+</template>
+
+<script>
+export default {
+  name: "HomePage"
+}
+</script>
+
+<style scoped>
+.container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+</style>
